a9b319c6d845543a3defb86f0bec70cf9d0a6f1f
taniatressa <taniatressa.thomas@gmail.com>
2016-12-06 17:16:27 -0800

    added coffeScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
+//gulp coffe to covert coffe script
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-  gutil.log('learning Workflows');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+//procees coffe d=script
+//add src and pipe to coffe var
+// bare - complies js without putting it on safety wrapper
+//once we finish processing put it to dest folder
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
+
