fa6e27301ed8558463db7b7d04b7fecece026e12
taniatressa <taniatressa.thomas@gmail.com>
2016-12-06 20:53:21 -0800

    added minified html files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     compass = require('gulp-compass'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
     concat = require('gulp-concat');
 
 var env,
@@ -83,7 +84,7 @@ gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
-   gulp.watch(htmlSources, ['html']);
+  gulp.watch('build/development/*.html', ['html']);
   gulp.watch(jsonSources, ['json']);
 });
 
@@ -96,7 +97,9 @@ gulp.task('connect', function() {
 });
 //keep track of html
 gulp.task('html', function() {
-  gulp.src(htmlSources)
+  gulp.src('build/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
     .pipe(connect.reload())
 });
 //track of json changes
